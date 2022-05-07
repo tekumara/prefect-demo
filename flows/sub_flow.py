8ed9f01b3c1bc928f65b36412b73588a03a991b8
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2022-05-07 23:24:26 +1000

    feat: add sub_flow example

---
+++ flows/sub_flow.py
@@ -0,0 +1,25 @@
+from prefect import flow, get_run_logger
+
+
+@flow
+def common_flow(config: dict) -> int:
+    # show in prefect ui
+    logger = get_run_logger()
+    logger.info("I am a subgraph that shows up in lots of places!")
+    intermediate_result = 42
+    return intermediate_result
+
+
+@flow
+def main_flow() -> None:
+    # do some things
+    # then call another flow function
+
+    data = common_flow(config={})
+    logger = get_run_logger()
+    logger.info(f"data = {data}")
+    # do more things
+
+
+if __name__ == "__main__":
+    main_flow()
