b3aeb391e2943082499aad38b25b875847aff777
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2022-05-07 16:51:51 +1000

    fix: fsspec config so it can be sourced in zsh

--- config/fsspec-env.sh
+++ config/fsspec-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FSSPEC_CONFIG_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+FSSPEC_CONFIG_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]:-$0}")")"
 AWS_ACCESS_KEY_ID=$(kubectl get secret --namespace default minio -o jsonpath="{.data.root-user}" | base64 --decode)
 AWS_SECRET_ACCESS_KEY=$(kubectl get secret --namespace default minio -o jsonpath="{.data.root-password}" | base64 --decode)
 
