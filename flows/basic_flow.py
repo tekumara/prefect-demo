a476bf49021ec1e8621d980bdccf61064cf69c69
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2022-06-13 18:46:04 +1000

    feat: add task to basic flow and use logging

--- flows/basic_flow.py
+++ flows/basic_flow.py
@@ -1,18 +1,25 @@
-from prefect import State, flow, get_run_logger
+from prefect import State, flow, get_run_logger, task
 from prefect.flows import Flow
+from prefect.futures import PrefectFuture
+from prefect.utilities.asyncio import Sync
 
 
 @flow
-def add_one(i: int) -> int:
+def add_one_with_logging(i: int) -> PrefectFuture[int, Sync]:
     # logger requires a flow or task run context
     logger = get_run_logger()
     logger.info(f"{i=}")
+    return add_one(i)
+
+
+@task
+def add_one(i: int) -> int:
     return i + 1
 
 
 if __name__ == "__main__":
     # to demonstrate that this is a Flow object
-    f: Flow = add_one
+    f: Flow = add_one_with_logging
 
     # execute Flow
-    r: State[int] = f(1)
+    r: State[PrefectFuture[int, Sync]] = f(1)
--- tests/test_basic_flow.py
+++ tests/test_basic_flow.py
@@ -1,15 +1,18 @@
 from prefect import State
+from prefect.futures import PrefectFuture
 from prefect.orion.schemas.states import StateType
+from prefect.utilities.asyncio import Sync
 
-from flows.basic_flow import add_one
+from flows.basic_flow import add_one, add_one_with_logging
 
 
+# works as long as the task does not use anything from the prefect context (eg: loggers)
 def test_underlying_fn():
     assert add_one.fn(41) == 42
 
 
-def test_with_state():
-    state: State[int] = add_one(41)
+def test_flow():
+    state: State[PrefectFuture[int, Sync]] = add_one_with_logging(41)
     assert state.type == StateType.COMPLETED
 
-    assert state.result() == 42
+    assert state.result().result() == 42
