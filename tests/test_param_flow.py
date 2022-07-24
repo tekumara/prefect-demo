b4cd256f552dfbe0fa8a161cd8f3a181c625b329
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2022-07-24 15:42:08 +1000

    build: use prefect_test_harness

--- tests/test_param_flow.py
+++ tests/test_param_flow.py
@@ -1,8 +1,18 @@
+import prefect.testing.utilities
 import pytest
 from prefect import State
 from prefect.orion.schemas.states import StateType
 
 from flows.param_flow import add_one, increment
+
+
+@pytest.fixture(scope="session")
+def prefect_test_harness():
+    # run flows against a temporary SQLite database rather than  ~/.prefect/orion.db
+    # adds an extra second to test time
+    # see https://orion-docs.prefect.io/tutorials/testing/
+    with prefect.testing.utilities.prefect_test_harness():
+        yield
 
 
 # works as long as the task does not use anything from the prefect context (eg: loggers)
@@ -11,14 +21,14 @@ def test_underlying_fn():
 
 
 @pytest.mark.skip(reason="https://github.com/PrefectHQ/prefect/issues/6049")
-def test_increment():
+def test_increment(prefect_test_harness):
     meaning_of_life_future = increment(41)
 
     assert meaning_of_life_future.get_state().type == StateType.COMPLETED
     assert meaning_of_life_future.get_state().result() == 42
 
 
-def test_increment_run():
+def test_increment_run(prefect_test_harness):
     state = increment._run(42)
     assert state.type == StateType.COMPLETED
 
