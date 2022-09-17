79137b40b718daffee62b3f0ffbdc0a0a7b9f884
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2022-09-17 11:56:33 +1000

    refactor: tests

--- tests/test_param_flow.py
+++ tests/test_param_flow.py
@@ -20,18 +20,9 @@ def test_underlying_fn():
     assert add_one.fn(41) == 42
 
 
-@pytest.mark.skip(reason="https://github.com/PrefectHQ/prefect/issues/6049")
 def test_increment(prefect_test_harness):
-    meaning_of_life_future = increment(41)
+    state: State = increment(42)    # type: ignore
 
-    assert meaning_of_life_future.get_state().type == StateType.COMPLETED
-    assert meaning_of_life_future.get_state().result() == 42
-
-
-def test_increment_run(prefect_test_harness):
-    state = increment._run(42)
     assert state.type == StateType.COMPLETED
+    assert state.result() == 43
 
-    # not sure why the result is itself a state object with the actual result?
-    assert type(state.result()) == State
-    assert state.result().result() == 43
