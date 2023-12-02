00fb199e102d7297b14c56d15a299e8431bcf6cc
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2023-12-02 18:15:32 +1100

    chore: bump non-prefect deps

---
+++ flows/run.py
@@ -0,0 +1,14 @@
+import asyncio
+
+from prefect.client.schemas.objects import FlowRun
+from prefect.deployments.deployments import run_deployment
+from prefect.server.schemas.states import StateType
+
+
+async def main():
+    deployments = ["param/yaml", "retry/yaml", "dask-kubes/python", "parent/python"]
+
+    flow_runs: list[FlowRun] = await asyncio.gather(*[run_deployment(name=d) for d in deployments])
+
+    for f in flow_runs:
+        assert f.state_type == StateType.COMPLETED
